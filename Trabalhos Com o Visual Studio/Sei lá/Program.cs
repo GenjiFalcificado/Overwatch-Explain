5bf8ab6261b06ce568d6fcc21a84be730a5ba2ce
GenjiFalcificado <162351349+GenjiFalcificado@users.noreply.github.com>
2024-03-19 21:41:34 -0300

    Add files via upload

---
+++ Trabalhos Com o Visual Studio/Sei lá/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sei_lá
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
